161cf1f15b3a8e9de31c1ceb1d03f0ace6672e00
seo31 <77620107+seo31@users.noreply.github.com>
2021-01-19 13:28:36 +0900

    Update moong00.py

--- Baekjoon/Greedy/miss bracket_1541/moong00.py
+++ Baekjoon/Greedy/miss bracket_1541/moong00.py
@@ -2,6 +2,7 @@ quest = input().split("-")
 result = 0
 for i in quest[0].split("+"):
     result += int(i)
-for y in quest[1].split("+"):
-    result -= int(y)
+for i in quest[1:]:
+    for j in i.split("+"):
+    result -= int(j)
 print(result)
