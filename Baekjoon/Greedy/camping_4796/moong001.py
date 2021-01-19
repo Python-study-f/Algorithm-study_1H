b429bef34ff70adfd47b4d52d1ea79f8ca609432
seo31 <77620107+seo31@users.noreply.github.com>
2021-01-19 13:11:10 +0900

    Update moong001.py

--- Baekjoon/Greedy/camping_4796/moong001.py
+++ Baekjoon/Greedy/camping_4796/moong001.py
@@ -4,5 +4,8 @@ while True:
     L, P, V = map(int, input().split()) 
     if L==0 and P==0 and V==0:
         break
-    result = (V//P*L) + (V%P)
+    if V%P >= L:
+        result = V//P*L + L
+    else:
+        result = (V//P*L) + (V%P)
     print("Case" + str(count) + ":" + str(result))
