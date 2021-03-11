a1c4f124c927bf4a893105b6f973c6174ab53082
MUREVIVE <51858239+MUREVIVE@users.noreply.github.com>
2021-03-11 22:38:46 +0900

    Update 2164_210311_realedu.py

--- Baekjoon/Deque/Card2_2164/2164_210311_realedu.py
+++ Baekjoon/Deque/Card2_2164/2164_210311_realedu.py
@@ -1,3 +1,17 @@
+"""
+1. 제일 위 카드는 버린다.
+2. 제일 위 카드를 제일 아래 카드 밑으로 옮긴다.
+	
+1, 2를 반복하여 마지막에 남는 카드는?
+
+==> collections의 deque 활용
+	- d 변수 deque() 선언
+	- N 길이 만큼 d에 append
+	- deque.popleft(), deque.append() 하고자 했으나 
+	append()의 경우 popleft()를 한번 더 해야하고, 그렇게 되면 예외적인 경우들로 인해 해결이 힘들어짐.
+그래서 rotate(-1)을 이용하여 왼쪽으로 회전하는 것을 이용.
+"""
+
 from collections import deque
 import sys
 
