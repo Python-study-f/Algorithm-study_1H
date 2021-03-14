1e701294c3db65b894097ca498db8625cd706a3c
Jeung Hyun Byun <peterbyun96@gmail.com>
2021-03-14 17:53:39 +0900

    Create 771_210314_jeung.py

---
+++ Leetcode/Hash_Table/Longest_Substring_Without_Repeating_Characters_3/771_210314_jeung.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution1:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_t = 0
+        for each_i in range(len(s)):
+            # tmp_s = ''
+            existing = []
+
+            t = 0
+            curr_s = s[each_i + t]
+            while curr_s not in existing:
+                existing.append(curr_s)
+                t += 1
+                if t + each_i >= len(s):
+                    break
+
+                curr_s = s[each_i+t]
+
+            if t > max_t:
+                max_t = t
+
+        return max_t
+
+
+solution = Solution1()
+print(solution.lengthOfLongestSubstring('abcabcbb'))
+print(solution.lengthOfLongestSubstring('bbbbb'))
+print(solution.lengthOfLongestSubstring('pwwkew'))
