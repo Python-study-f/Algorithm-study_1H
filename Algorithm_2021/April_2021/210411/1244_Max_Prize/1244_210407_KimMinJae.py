0e8e1d0f3ba2920c3416eba39add79c7543aa0ad
Jannakim <baradamoh@gmail.com>
2021-04-09 15:30:39 +0900

    1244_210409_KimMinJae

--- Algorithm_2021/April_2021/210411/1244_Max_Prize/1244_210407_KimMinJae.py
+++ Algorithm_2021/April_2021/210411/1244_Max_Prize/1244_210407_KimMinJae.py
@@ -1,122 +1,45 @@
-# TLE
+# AC
 for t in range(1,int(input())+1):
     num, k= input().split()
     k=int(k)
-    L=sorted(list(num),reverse=True)
-    le= len(L)
-    mx= int(''.join(L))
-    ans=0
-    remain=1e9
-    ls=[0]*10
-    dic={}
-    for i in range(le):
-        if num[i] not in dic:
-            dic[num[i]]=[]
-        dic[num[i]].append(i)
+    num=list(num)
+    n= len(num)
+    mx_val= int(''.join(sorted(list(num),reverse=True)))
+    
+    chk= {}
+    mx=[-1,1]
+    def backtrack(cnt,num):
+        global mx, mx_val, n;
+        ans= int(''.join(num))
+        if ans==mx_val: #
+            if mx[0]<ans:
+                mx= [ans,cnt]
+            else:
+                if mx[1]%2:
+                    mx= [ans,cnt]
+        if not cnt: #
+            if ans==mx[0]:
+                mx[1]= cnt
+            elif ans>mx[0]:
+                mx= [ans, cnt]
+            return True
+        for i in range(n):
+            for j in range(n):
+                if i!=j:
+                    nxt= num[:]
+                    nxt[i], nxt[j]= nxt[j], nxt[i]
+                    new= int(''.join(nxt))
+                    if new>=ans and (new,cnt-1) not in chk:
+                        chk[(new,cnt-1)]=1
+                        backtrack(cnt-1, nxt)
+    
+    backtrack(k, num)
 
-    for el in num:
-        ls[int(el)]+=1
-    def backtrack(THS,cnt):
-        nm= int(''.join(THS))
-        print(nm, cnt)
-        global ans, remain
-        remain= min(remain,cnt)
-        ans= max(ans, nm)
-        if nm==mx:
-            
-            return
-        if not cnt:
-            return
-        
-        global le
-        for idx, (a, b) in enumerate(zip(THS, L)):
-            if a!=b:
-                for k in dic[b]:
-                    ths=THS[:]
-                    #print(idx, k)
-                    if idx<k:
-                        ths[k], ths[idx]= ths[idx], ths[k]
-                        backtrack(ths,cnt-1)
-                break
-    backtrack(list(num), k)
-    #print(remain, ans)
     print(f'#{t}',end=' ')
-    if not remain%2:
-        print(ans)
-    else:
-        a= ans//100*100
-        b= ans%100//10
-        c= ans%10*10
+    if mx[1]%2:
+        a= mx[0]//100*100
+        b= mx[0]%100//10
+        c= mx[0]%10*10
         print(a+b+c)
-
-
-
-
-
-
-'''
-for t in range(1,int(input())+1):
-    num, k= input().split()
-    k=int(k)
-    L=[]
-    for idx, el in enumerate(num):
-        L.append((el,idx))
-    M=L[:]
-    L.sort()
-
-    if list(num)==sorted(num,reverse=True):
-        ans=sorted(num,reverse=True)
-        if not k%2:
-            print(''.join(ans))
-        else:
-            ans[-1], ans[-2]= ans[-2], ans[-1]
-            print(''.join(ans))
-        continue
-    top=len(num)-1
-    left=0
-    right=top
-    ans=[0]*(top+1)
-    ls=[]
-    chk=[False]*(top+1)
-    while left<=right and k:
-        if chk[L[right][1]]==True:
-            right-=1
-            continue
-        if chk[M[left][1]]==True or M[left][0]==L[right][0] :
-            left+=1
-            continue
-
-        k-=1
-        if left==right:
-            ls.append(M[left][0])
-            chk[M[left][1]]=True
-            k+=1
-        else:
-            ls.append(M[left][0])
-            ls.append(L[right][0])
-            chk[L[right][1]]=True
-            chk[M[left][1]]=True
-        left+=1
-        right-=1
-    
-    ls.sort()
-    #print(ls)
-    #print(chk)
-    #print(k)
-    print(f'#{t}',end=' ')
-    for i in range(top+1):
-        if chk[i]:
-            ans[i]=ls.pop()
-        else:
-            ans[i]=num[i]
-    if not k%2:
-        print(''.join(ans))
     else:
-        ans[-1], ans[-2]= ans[-2], ans[-1]
-        print(''.join(ans))
-    
-
-
-
-
-'''
+        print(mx[0])
