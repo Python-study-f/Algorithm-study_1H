1683b021cb0fc4dd4482d2600f8c9ab508b1f2b6
jjong0416-tech <73544708+jjong0416-tech@users.noreply.github.com>
2021-04-19 16:25:17 +0900

    Update 79_210419_asura.py

--- Algorithm_2021/April_2021/210425/79_Word_Search/79_210419_asura.py
+++ Algorithm_2021/April_2021/210425/79_Word_Search/79_210419_asura.py
@@ -1,21 +1,23 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         visited = [[False] * len(board[0]) for _ in range(len(board))]
-
-        def dfs(board, start, x, y, target, visit):
+        
+        def dfs(start,x,y,target,visit):
+            
+            
 
             if start == len(target):
                 return True
 
             if x < 0 or x >= len(board) or y < 0 or y >= len(board[0]) or board[x][y] != word[start]:
                 return False
-
+            
             if visit[x][y]:
                 return
 
             visit[x][y] = True
-            result = dfs(board, start + 1, x - 1, y, target, visit) or dfs(board, start + 1, x + 1, y, target, visit) or \
-                     dfs(board, start + 1, x, y - 1, target, visit) or dfs(board, start + 1, x, y + 1, target, visit)
+            result = dfs(start+1,x-1,y,target,visit) or dfs(start+1,x+1,y,target,visit) or \
+                     dfs( start+1,x,y-1,target,visit) or dfs(start+1,x,y+1,target,visit)
             visit[x][y] = False
 
             return result
@@ -24,7 +26,7 @@ class Solution:
 
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if dfs(board, 0, i, j, word, visited):
+                if dfs(0,i,j,word,visited):
                     return True
 
         return False
