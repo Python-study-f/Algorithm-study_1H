9425def67adeafeaa83759ee4599eb8f831a6cdd
yonery <yonery9@gmail.com>
2021-04-30 12:55:57 +0900

    Create 329_Longest Increasing_Path_in_a_Matrix_hyeri.py

---
+++ Algorithm_2021/May_2021/210502/329_Longest Increasing_Path_in_a_Matrix/329_Longest Increasing_Path_in_a_Matrix_hyeri.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        dx = [0, 0, 1, -1]
+        dy = [1, -1, 0, 0]
+        result = 0
+        visit = [[1]*len(matrix[0]) for _ in range(len(matrix))]
+        
+        def dfs(x, y, n):   
+            ans = n
+            #print(str(x)+ " "+str(y)+" : "+str(n))
+            for d in range(4):
+                tx = x + dx[d]
+                ty = y + dy[d]
+                if tx < 0 or ty < 0 or tx >= len(matrix) or ty >= len(matrix[0]):
+                    continue
+                if matrix[tx][ty] > matrix[x][y] and visit[tx][ty] <= visit[x][y] + 1:
+                    visit[tx][ty] = visit[x][y] + 1
+                    ans = max(dfs(tx, ty, n + 1), ans)
+            return ans
+                    
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                result = max(dfs(i, j, 1), result)
+        return result
+            
