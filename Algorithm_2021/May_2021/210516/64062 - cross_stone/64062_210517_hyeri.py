6c2676694136ed54291801698547f3abaa1a14d3
yonery <yonery9@gmail.com>
2021-05-17 12:40:04 +0900

    Create 64062_210517_hyeri.py

---
+++ Algorithm_2021/May_2021/210516/64062 - cross_stone/64062_210517_hyeri.py
@@ -0,0 +1,19 @@
+def solution(stones, k):
+    answer = 0
+    dp = [0]*len(stones)
+    for i in range(k):
+        dp[i] = stones[i]
+        
+    for i in range(k, len(stones)):
+        dp[i] = 0
+        for j in range(1, k+1):
+            dp[i] = max(dp[i], dp[i-j])
+        if stones[i] < dp[i]:
+            dp[i] = stones[i]
+        
+    for i in range(1, k+1):
+        answer = max(answer, dp[len(stones)-i])
+    return answer
+  
+ # O(n) 이 안 되서 당황스럽;; 정확성 100.0 효율성 0.0 점 코드 => 총 64.1
+ # 이진탐색을 적용해서 다시 풀어 볼 예정
