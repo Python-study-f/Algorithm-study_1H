fe9065df8d22d36d71725b6dca6d45d09d8e4531
yonery <yonery9@gmail.com>
2021-05-17 13:26:12 +0900

    Update 20164_210517_hyeri.py

--- Algorithm_2021/May_2021/210516/20164 - odd_holic/20164_210517_hyeri.py
+++ Algorithm_2021/May_2021/210516/20164 - odd_holic/20164_210517_hyeri.py
@@ -1,22 +1,20 @@
 N = input()
 
-def dfs(num, n, ok):    # ok 가 True : 최댓값, False : 최솟값
-    if len(num) == 1:
+def dfs(num, n, ok):        # ok True : 최댓값, False : 최솟값
+    if len(num) == 1:       
         if int(num[0]) % 2 != 0:
             n += 1
         return n
     elif len(num) == 2:
-        ans = 0
         if int(num[0]) % 2 != 0:
-            ans += 1
+            n += 1
         if int(num[1]) % 2 != 0:
-            ans += 1
-        return dfs(str(int(num[0]) + int(num[1])), n + ans, ok)
+            n += 1
+        return dfs(str(int(num[0]) + int(num[1])), n, ok)
     else:
-        answer = 0
         for i in range(len(num)):
             if int(num[i]) % 2 != 0:
-                answer += 1
+                n += 1
         result = 0
         if ok:
             result = 0
@@ -28,10 +26,9 @@ def dfs(num, n, ok):    # ok 가 True : 최댓값, False : 최솟값
                 b = int(num[i:j])
                 c = int(num[j:])
                 if ok:
-                    result = max(result, dfs(str(a + b + c), n + answer, ok))
+                    result = max(result, dfs(str(a + b + c), n, ok))
                 else:
-                    result = min(result, dfs(str(a + b + c), n + answer, ok))
+                    result = min(result, dfs(str(a + b + c), n, ok))
         return result
-
 
 print("{} {}".format(dfs(N, 0, False), dfs(N, 0, True)))
