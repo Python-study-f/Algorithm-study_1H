53a2aed96460f277120ad031021b5832162b92a2
yonery <yonery9@gmail.com>
2021-05-25 15:43:00 +0900

    Create 2468_210525_hyeri.py

---
+++ Algorithm_2021/May_2021/210530/2468 - safe_area/2468_210525_hyeri.py
@@ -0,0 +1,41 @@
+import sys
+sys.setrecursionlimit(100000)
+
+N = int(input())
+mp = [list(map(int, input().split())) for _ in range(N)]
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+mx = 0
+for i in range(N):
+    for j in range(N):
+        mx = max(mx, mp[i][j])
+result = 0
+visit = [[0] * N for _ in range(N)]
+
+
+def dfs(x, y, n, K):
+    for d in range(4):
+        tx = x + dx[d]
+        ty = y + dy[d]
+        if tx < 0 or ty < 0 or tx >= N or ty >= N:
+            continue
+        if visit[tx][ty] == 0 and mp[tx][ty] > K:
+            visit[tx][ty] = n
+            dfs(tx, ty, n, K)
+
+
+for k in range(mx + 1):
+    num = 1
+    visit = [[0] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if visit[i][j] == 0 and mp[i][j] > k:
+                visit[i][j] = num
+                dfs(i, j, num, k)
+                num += 1
+    num -= 1
+    result = max(result, num)
+
+print(result)
