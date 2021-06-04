0cf132f41407f8d5912c642bb2baceaed0f95216
Park Jong Chan <73544708+JJong0416@users.noreply.github.com>
2021-06-04 21:12:14 +0900

    Update 15724_210601_asura.py

--- Algorithm_2021/June_2021/210606/15724 - Adjust Hiking/15724_210601_asura.py
+++ Algorithm_2021/June_2021/210606/15724 - Adjust Hiking/15724_210601_asura.py
@@ -1,14 +1,41 @@
-N, M = map(int,input().split())
-board = [list(map(int,input().split())) for _ in range(N)]
-
-dp = [[0] * (M+1) for _ in range(N+1)]
-
-for i in range(1,N+1):
-    for j in range(1,M+1):
-        dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + board[i-1][j-1]
+dx = [0,0,-1,1] # L,R,U,D
+dy = [-1,1,0,0]
 
 
-K = int(input())
-for tc in range(K):
-    x1,y1,x2,y2 = list(map(int,input().split()))
-    print(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1])
+def dfs(x,y,change,tot):
+    global ans,K
+    ans = max(ans,tot)
+    visit[x][y] = True
+
+    for d in range(4):
+        nx, ny = x + dx[d], y + dy[d]
+
+        if 0 <= nx < N and 0 <= ny < N:
+            if not visit[nx][ny]:
+                if board[nx][ny] < board[x][y]:
+                    dfs(nx,ny,change,tot+1)
+                elif not change and (board[nx][ny] - board[x][y] < K):
+                    tmp = board[nx][ny]
+                    board[nx][ny] = board[x][y] - 1
+                    dfs(nx,ny,True,tot+1)
+                    board[nx][ny] = tmp
+    visit[x][y] = False
+
+
+
+TC = int(input())
+
+for tc in range(1,TC+1):
+    N,K = map(int,input().split())
+    board = [list(map(int,input().split())) for _ in range(N)]
+
+    height_max = max(map(max,board)) # 최대높이 구하기. 이거 어떻게하면 효율적으로 구할까요..
+
+    ans = 1
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == height_max:
+                visit = [[False] * N for _ in range(N)]
+                dfs(i,j,False,1)
+
+    print(f'#{tc} {ans}')
