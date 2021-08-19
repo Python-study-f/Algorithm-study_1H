7fbb01b89b1ac3535c30025b37b6edb9ea885aa3
Park Jong Chan <73544708+JJong0416@users.noreply.github.com>
2021-08-19 18:35:31 +0900

    Update 14_210329_asura.py

--- Algorithm_2021/April_2021/210404/14_Longest_Common_Prefix/14_210329_asura.py
+++ Algorithm_2021/April_2021/210404/14_Longest_Common_Prefix/14_210329_asura.py
@@ -10,7 +10,7 @@
 #                 for j in range(1, len(strs)):
 #                     if strs[0][i] != strs[j][i]:
 #                         return strs[0][:i]
-#         return strs[0]
+#         return strs[0]..
 
 
 
